4dff148b2e3cc9c8c01182ad48a53f477db5ab6b
unknown <kbigdae@gmail.com>
2019-07-09 23:42:52 +0900

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+#    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
