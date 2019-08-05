c3b7ae43ddf6fe38b8c46f1dbc3970e35fea567a
unknown <kbigdae@gmail.com>
2019-08-05 23:35:54 +0900

    last

--- myapp/views.py
+++ myapp/views.py
@@ -38,11 +38,11 @@ def loadDocument(region):
     result = None
     distinct = Document.objects.values('place').annotate(max_id=Max('id'))
     if region != "":
-      #result = Document.objects.filter(time_calc__gte=datetime.datetime.now()).filter(id__in=[i['max_id'] for i in distinct.all()]).filter(region=region)
-      result = Document.objects.filter(id__in=[i['max_id'] for i in distinct.all()]).filter(region=region)
+      result = Document.objects.filter(time_calc__gte=datetime.datetime.now()).filter(id__in=[i['max_id'] for i in distinct.all()]).filter(region=region)
+      #result = Document.objects.filter(id__in=[i['max_id'] for i in distinct.all()]).filter(region=region)
     else:
-      #result = Document.objects.filter(time_calc__gte=datetime.datetime.now()).filter(id__in=[i['max_id'] for i in distinct.all()])
-      result = Document.objects.filter(id__in=[i['max_id'] for i in distinct.all()])
+      result = Document.objects.filter(time_calc__gte=datetime.datetime.now()).filter(id__in=[i['max_id'] for i in distinct.all()])
+      #result = Document.objects.filter(id__in=[i['max_id'] for i in distinct.all()])
     return result
 
 def resize(file):
